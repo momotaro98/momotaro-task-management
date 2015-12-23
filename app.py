4f2b1c7ae08a9dab5b554db15cb92cb42025e7d0
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2015-12-23 17:27:30 +0900

    DONE Page fundamental func implemented

--- app.py
+++ app.py
@@ -31,25 +31,36 @@ def index():
 @app.route('/done', methods=['POST', 'GET'])
 def done_page():
     if request.method == 'POST':
+        task_title = request.form["task_title"]
         set_time = int(request.form["set_time"]) # 設定時間
         start_hour = int(request.form["Hours"]) # 開始時間 時
         start_minute = int(request.form["Minutes"]) # 開始時間 分
         remained_time = int(request.form["remained_time"]) # 残り時間
-        serial_passed_time = int(request.form["serial_passed_time"]) # 経過時間
-        task_title = request.form["task_title"]
+        serial_passed_time = -1 * int(request.form["serial_passed_time"]) # 経過時間
 
-        done_date = datetime.date.today()
+        done_datetime = datetime.datetime.now()
         set_hour = set_time // 3600
         set_minute = (set_time % 3600) // 60
+        serial_passed_hour = serial_passed_time // 3600
+        serial_passed_minute = (serial_passed_time % 3600) // 60
+
+        over_time = serial_passed_time - set_time
+        over_hour = over_time // 3600
+        over_minute = (over_time % 3600) // 60
 
         return render_template('done.html',
-                            done_date=done_date,
+                            done_datetime=done_datetime,
+                            task_title=task_title,
                             set_hour=set_hour,
                             set_minute=set_minute,
                             start_hour=start_hour,
                             start_minute=start_minute,
-                            serial_passed_time=serial_passed_time,
-                            task_title=task_title)
+                            serial_passed_hour=serial_passed_hour,
+                            serial_passed_minute=serial_passed_minute,
+                            over_time=over_time,
+                            over_hour=over_hour,
+                            over_minute=over_minute
+                            )
 
     return render_template('setting.html')
 
