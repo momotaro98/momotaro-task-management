903c95d6ea866fb4c119c2eb1735f4ffed076909
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2015-12-23 14:32:59 +0900

    bug fixed?

--- app.py
+++ app.py
@@ -30,26 +30,20 @@ def index():
 @app.route('/done', methods=['POST', 'GET'])
 def done_page():
     if request.method == 'POST':
-        set_time = int(request.form["set_time"])
-        Hours = int(request.form["Hours"])
-        Minutes = int(request.form["Minutes"])
-        remained_time = int(request.form["remained_time"])
-        serial_passed_time = int(request.form["serial_passed_time"])
+        set_time = int(request.form["set_time"]) # 設定時間
+        start_hour = int(request.form["Hours"]) # 開始時間 時
+        start_minute = int(request.form["Minutes"]) # 開始時間 分
+        remained_time = int(request.form["remained_time"]) # 残り時間
+        serial_passed_time = int(request.form["serial_passed_time"]) # 経過時間
 
         set_hour = set_time // 3600
         set_minute = (set_time % 3600) // 60
-        if set_hour and set_minute:
-            set_time_output = "設定時間 {0} 時間 {1} 分".format(set_hour, set_minute)
-        elif set_hour:
-            set_time_output = "設定時間 {0} 時間".format(set_hour)
-        else:
-            set_time_output = "設定時間 {0} 分".format(set_minute)
 
-        start_time = "{0} 時 {1} 分".format(Hours, Minutes)
     return render_template('done.html',
-                        set_time_output=set_time_output,
-                        start_time=start_time,
-                        remained_time=remained_time,
+                        set_hour=set_hour,
+                        set_minute=set_minute,
+                        start_hour=start_hour,
+                        start_minute=start_minute,
                         serial_passed_time=serial_passed_time)
 
 if __name__ == '__main__':
